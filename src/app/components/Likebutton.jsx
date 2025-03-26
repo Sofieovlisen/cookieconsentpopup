9cff5d368a77d122823e08a52b1f77ba47f37af1
Sofie <Soov0001@stud.kea.dk>
2025-03-26 21:37:54 +0100

    cookie

---
+++ src/app/components/Likebutton.jsx
@@ -0,0 +1,14 @@
+"use client";
+import { useState } from "react";
+
+const Likebutton = (props) => {
+  const [likes, setLikes] = useState(0);
+  return (
+    <button onClick={() => setLikes(likes + 1)}>
+      {props.label}
+      {likes > 1 ? "s" : ""} {likes}
+    </button>
+  );
+};
+
+export default Likebutton;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
+import { LiaCookieBiteSolid } from "react-icons/lia";
+import { IoCloseOutline } from "react-icons/io5";
 
-
+import Likebutton from "@/app/components/Likebutton";
 export default function Home() {
   return (
-    <div>
-      hej
+    <div className="div">
+      <div className="icons">
+        <LiaCookieBiteSolid
+          style={{ fontSize: "56px", rotate: "-90deg", color: "white" }}
+        ></LiaCookieBiteSolid>
+        <IoCloseOutline
+          style={{
+            display: "flex",
+            alignSelf: "flex-start",
+            color: "white",
+            justifyContent: "flex-end",
+            fontSize: "20px",
+          }}
+        ></IoCloseOutline>
+      </div>
+      <p>We use cookies to improve your user experience.</p>
+      <button>I like Cookies</button>
     </div>
   );
 }
