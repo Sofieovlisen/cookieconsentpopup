53bb57671d83caae41d2852fb3a4d6615d50b120
Sofie <Soov0001@stud.kea.dk>
2025-03-28 12:42:13 +0100

    mere

---
+++ src/app/components/Cookie.jsx
@@ -0,0 +1,29 @@
+// import { LiaCookieBiteSolid } from "react-icons/lia";
+// import { IoCloseOutline } from "react-icons/io5";
+
+// import Likebutton from "@/app/components/Likebutton";
+
+// const Cookie = () => {
+//   return (
+//     <div className="div">
+//       <div className="icons">
+//         <LiaCookieBiteSolid
+//           style={{ fontSize: "56px", rotate: "-90deg", color: "white" }}
+//         ></LiaCookieBiteSolid>
+//         <IoCloseOutline
+//           style={{
+//             display: "flex",
+//             alignSelf: "flex-start",
+//             color: "white",
+//             justifyContent: "flex-end",
+//             fontSize: "20px",
+//           }}
+//         ></IoCloseOutline>
+//       </div>
+//       <p>We use cookies to improve your user experience.</p>
+//       <Likebutton label="I like Cookies"></Likebutton>
+//     </div>
+//   );
+// };
+
+// export default Cookie;
