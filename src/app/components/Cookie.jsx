1cf9b68ae591e52de4e8c304d743f427e6374a3c
Sofie <Soov0001@stud.kea.dk>
2025-03-28 13:13:05 +0100

    state

--- src/app/components/Cookie.jsx
+++ src/app/components/Cookie.jsx
@@ -1,29 +1,36 @@
-// import { LiaCookieBiteSolid } from "react-icons/lia";
-// import { IoCloseOutline } from "react-icons/io5";
+"use client";
+import { useState } from "react";
 
-// import Likebutton from "@/app/components/Likebutton";
+import { LiaCookieBiteSolid } from "react-icons/lia";
+import { IoCloseOutline } from "react-icons/io5";
 
-// const Cookie = () => {
-//   return (
-//     <div className="div">
-//       <div className="icons">
-//         <LiaCookieBiteSolid
-//           style={{ fontSize: "56px", rotate: "-90deg", color: "white" }}
-//         ></LiaCookieBiteSolid>
-//         <IoCloseOutline
-//           style={{
-//             display: "flex",
-//             alignSelf: "flex-start",
-//             color: "white",
-//             justifyContent: "flex-end",
-//             fontSize: "20px",
-//           }}
-//         ></IoCloseOutline>
-//       </div>
-//       <p>We use cookies to improve your user experience.</p>
-//       <Likebutton label="I like Cookies"></Likebutton>
-//     </div>
-//   );
-// };
+const Cookie = () => {
+  const [cookiesAccepted, setCookiesAccepted] = useState(false);
+  return (
+    !cookiesAccepted && (
+      <div className="div">
+        <div className="icons">
+          <LiaCookieBiteSolid
+            style={{ fontSize: "56px", rotate: "-90deg", color: "white" }}
+          ></LiaCookieBiteSolid>
+          <IoCloseOutline
+            style={{
+              display: "flex",
+              alignSelf: "flex-start",
+              color: "white",
+              justifyContent: "flex-end",
+              fontSize: "20px",
+            }}
+          ></IoCloseOutline>
+        </div>
+        <p>We use cookies to improve your user experience.</p>
+        <button onClick={() => setCookiesAccepted(!cookiesAccepted)}>
+          {" "}
+          I like cookies
+        </button>
+      </div>
+    )
+  );
+};
 
-// export default Cookie;
+export default Cookie;
--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,36 @@
-import { LiaCookieBiteSolid } from "react-icons/lia";
-import { IoCloseOutline } from "react-icons/io5";
+// "use client";
+// import { useState } from "react";
+
+// import { LiaCookieBiteSolid } from "react-icons/lia";
+// import { IoCloseOutline } from "react-icons/io5";
 
 import Likebutton from "@/app/components/Likebutton";
+import Cookie from "./components/Cookie";
 export default function Home() {
+  // const [cookiesAccepted, setCookiesAccepted] = useState(false);
   return (
-    <div className="div">
-      <div className="icons">
-        <LiaCookieBiteSolid
-          style={{ fontSize: "56px", rotate: "-90deg", color: "white" }}
-        ></LiaCookieBiteSolid>
-        <IoCloseOutline
-          style={{
-            display: "flex",
-            alignSelf: "flex-start",
-            color: "white",
-            justifyContent: "flex-end",
-            fontSize: "20px",
-          }}
-        ></IoCloseOutline>
-      </div>
-      <p>We use cookies to improve your user experience.</p>
-      <Likebutton label="I like Cookies"></Likebutton>
-    </div>
+    <Cookie></Cookie>
+    // <div className="div">
+    //   <div className="icons">
+    //     <LiaCookieBiteSolid
+    //       style={{ fontSize: "56px", rotate: "-90deg", color: "white" }}
+    //     ></LiaCookieBiteSolid>
+    //     <IoCloseOutline
+    //       style={{
+    //         display: "flex",
+    //         alignSelf: "flex-start",
+    //         color: "white",
+    //         justifyContent: "flex-end",
+    //         fontSize: "20px",
+    //       }}
+    //     ></IoCloseOutline>
+    //   </div>
+    //   <p>We use cookies to improve your user experience.</p>
+    //   <button onClick={() => setCookiesAccepted(!cookiesAccepted)}>
+    //     {" "}
+    //     I like cookies
+    //   </button>
+    //   {/* <Likebutton onClick={() => } label="I like Cookies"></Likebutton> */}
+    // </div>
   );
 }
