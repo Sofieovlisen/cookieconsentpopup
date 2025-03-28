b04f8c350de2ecb0447f9117aad4a12b5c55abb7
Sofie <Soov0001@stud.kea.dk>
2025-03-28 14:04:27 +0100

    css rettelse

--- src/app/components/CookieFooter.jsx
+++ src/app/components/CookieFooter.jsx
@@ -1,7 +1,7 @@
 "use client";
 const CookieFooter = ({ cookiesAccepted, setCookiesAccepted }) => {
   return (
-    <div>
+    <div className="cookie_footer">
       <p>We use cookies to improve your user experience.</p>
       <button onClick={() => setCookiesAccepted(!cookiesAccepted)}>
         {" "}
